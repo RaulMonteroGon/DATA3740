cb04f3aab4a5a0980d2af349dfe41eb4b1b129a2
Raúl Montero <rmontegon@gmail.com>
2021-09-12 20:17:13 +0200

    Basic activities + reorganization of the packages

--- app/src/main/java/com/example/data3740/project/landing/ModeActivity.kt
+++ app/src/main/java/com/example/data3740/project/landing/ModeActivity.kt
@@ -1,12 +1,13 @@
-package com.example.data3740
+package com.example.data3740.project.landing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.data3740.R
 
-class MainActivity : AppCompatActivity() {
+class ModeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_mode)
     }
 }
---
+++ app/src/main/java/com/example/data3740/project/landing/SelectImageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.data3740.project.landing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.data3740.R
+
+class SelectImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_image)
+    }
+}
