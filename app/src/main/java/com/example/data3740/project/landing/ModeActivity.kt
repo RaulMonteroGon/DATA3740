27b8de22e1b92721bf1876b44d4659a7eec4ec9a
Raúl Montero <rmontegon@gmail.com>
2021-09-14 17:20:28 +0200

    Buttons functionality

--- app/src/main/java/com/example/data3740/project/landing/ModeActivity.kt
+++ app/src/main/java/com/example/data3740/project/landing/ModeActivity.kt
@@ -2,12 +2,32 @@ package com.example.data3740.project.landing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.data3740.R
+import kotlinx.android.synthetic.main.activity_mode.*
+import android.content.Intent
+
 
 class ModeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mode)
+
+        val btnreselect : Button = findViewById(R.id.btn_reselect)
+        val btnsettings : Button = findViewById(R.id.btn_settings)
+
+
+        btnreselect.setOnClickListener {
+            val selectimg = Intent(this, SelectImageActivity::class.java)
+            startActivity(selectimg)
+            finish()
+        }
+        btnsettings.setOnClickListener {
+            val settings = Intent(this, SettingsActivity::class.java)
+            startActivity(settings)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/data3740/project/landing/SelectImageActivity.kt
+++ app/src/main/java/com/example/data3740/project/landing/SelectImageActivity.kt
@@ -1,7 +1,9 @@
 package com.example.data3740.project.landing
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.data3740.R
 
 class SelectImageActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class SelectImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_image)
+
+        val btnsettingsimg : Button = findViewById(R.id.btn_settingsimg)
+
+
+        btnsettingsimg.setOnClickListener {
+            val settings = Intent(this, SettingsActivity::class.java)
+            startActivity(settings)
+        }
     }
 }
